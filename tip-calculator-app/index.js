ab894a7c68cf2dcb2282b8eb343e44bd701d76b0
alextuchkov <alextychkoff@gmail.com>
2024-06-23 17:39:27 +0300

    adding styling to tip calculator

--- tip-calculator-app/index.js
+++ tip-calculator-app/index.js
@@ -1,27 +1,56 @@
-
-function handleSubmit() {
+function handleInput() {
     let total = document.getElementById("total").value;
     let numberPpl = document.getElementById("number-ppl").value;
     let definedTip = document.querySelector('input[name="defined-tip"]:checked');
     let customTip = document.getElementById('custom-tip').value;
+
+
+    if (definedTip) {
+        document.getElementById('custom-tip').value = '';
+    }
     
-    // results
+    if (customTip) {
+        resetDefinedTips()
+    }
+
+    if (total && numberPpl && (definedTip || customTip)) {
+        let tip = definedTip ? definedTip.value :  customTip;
+    calcAmount(total, numberPpl, tip ) 
+    }
+
+}
+
+function resetDefinedTips() {
+    let definedTipsInputs = document.querySelectorAll('input[name="defined-tip"]');
+    definedTipsInputs.forEach(item => {
+        item.checked = false;
+    });
+}
+
+
+function calcAmount(total, numberPpl, tip) {
+
     let tipResult = document.getElementById('total-tip');
     let totalPerPersonResult = document.getElementById('total-per-person');
     let totalTipSum;
 
-    // calculating Tip amount
-    if (definedTip) {
-        totalTipSum = Number(total)* Number(definedTip.value)/100;
-    } else if (definedTip == null && customTip) {
-        totalTipSum = ((Number(total)) * (Number(customTip)))/100;
+    if (tip == 0) {
+        totalTipSum = 0; 
     } else {
-        totalTipSum = 0;
+        totalTipSum = ((Number(total)) * (Number(tip)));
     }
 
-    // calc total amount
-  
     let totalAmountPerPerson = (Number(total) + totalTipSum) / Number(numberPpl);
     tipResult.innerText = totalTipSum;
     totalPerPersonResult.innerText = totalAmountPerPerson; 
+}
+
+function resetForm() {
+    document.getElementById("total").value = '';
+    document.getElementById("number-ppl").value = '';
+    resetDefinedTips();
+    document.getElementById('custom-tip').value = '';
+    document.getElementById('total-tip').innerText = '$0.00';
+    document.getElementById('total-per-person').innerText = '$0.00';
+
 }
